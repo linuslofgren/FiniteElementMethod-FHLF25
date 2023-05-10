8ae49d494f424e1b46eb7e3e4eb3dfcb8440b070
Linus von Ekensteen Löfgren <pnl.lofgren@gmail.com>
2023-05-10 14:27:16 +0200

    jippi

--- Project/main.py
+++ Project/main.py
@@ -12,15 +12,19 @@ D_cu = np.diag([k_cu, k_cu])
 k_ny = 0.26  # [W/(mK)]
 D_ny = np.diag([k_ny, k_ny])
 
-Tinf = 18  # [C]
-h = 1e5  # [W/m**2]
+T_inf = 18  # [C]
+h = -1e5  # [W/m**2]
 
 
+thickness = [0.001] # [m]
+t = thickness[0]
 # Definie Geometry
 
+# Mesh markers
+HEAT_FLUX_BOUNDARY = 2
 
 def define_geometry():
-    L = 0.005
+    L = 0.005 # [m]
     a = 0.1*L
     b = 0.1*L
     c = 0.3*L
@@ -47,7 +51,7 @@ def define_geometry():
         g.spline(s)
 
     for s in  [[18, 0]]:
-        g.spline(s, marker=2)
+        g.spline(s, marker=HEAT_FLUX_BOUNDARY)
 
     for s in [[17, 19], [19, 11]]:
         g.spline(s)
@@ -69,58 +73,50 @@ cfv.draw_geometry(g)
 
 
 coords, edof, dofs, bdofs, elementmarkers, boundary_elements = generate_mesh(g)
-
-node_lists = []
-for b in boundary_elements[2]:
-    node_lists.append(b.get("node-number-list"))
-print(node_lists)
-
 ex, ey = cfc.coord_extract(edof, coords, dofs)
 
 K = np.zeros((np.size(dofs), np.size(dofs)))
-thickness = [0.001]
-
 for eldof, elx, ely, materialindex in zip(edof, ex, ey, elementmarkers):
     Ke = cfc.flw2te(elx, ely, thickness, D_cu if materialindex == 1 else D_ny)
     cfc.assem(eldof, K, Ke)
-print(K)
+
+node_lists = []
+for b in boundary_elements[HEAT_FLUX_BOUNDARY]:
+    node_lists.append(b.get("node-number-list"))
+
+
+f_h = np.zeros([np.size(dofs), 1])
+
+for node_pair in node_lists:
+    r1 = None
+    r1_dof_index = None
+    r2 = None
+    r2_dof_index = None
+    for i, (coord, dof) in enumerate(zip(coords, dofs)):
+        if dof == node_pair[0]:
+            r1 = coord
+            r1_dof_index = i
+        if dof == node_pair[1]:
+            r2 = coord
+            r2_dof_index = i
+    
+    distance = np.linalg.norm(np.array(r1) - np.array(r2))
+
+    f_h[r1_dof_index] = -h*t * distance * 1/2
+    f_h[r2_dof_index] = -h*t * distance * 1/2
 
 
 
 bc = np.array([], 'i')
 bc_val = np.array([], 'i')
-f = np.array([1/2, 0, 1/2])
+
 cfv.draw_mesh(coords, edof, 1, 2)
 
-f = np.zeros([np.size(dofs), 1])
-
-heat_boundary_dofs = bdofs[2]
-print("--"*10)
-
-for i, (eldof, elx, ely) in enumerate(zip(edof, ex, ey)):
-    if eldof[0] in heat_boundary_dofs:
-        print(eldof, elx, ely)
-
-heat_bounds_dof = []
-t = 0.02
-print("-"*10)
-
-print(heat_boundary_dofs)
-for i, (dof, coord) in enumerate(zip(dofs, coords)):
-    if dof in heat_boundary_dofs:
-        f[i] = 0
-        print(coord)
-                # heat_bounds_dof.append([j, coord[1]])
-        f[i] = -h*t/2
-# print(f)
-print(f)
-a, r = cfc.solveq(K, f, np.array(bdofs.get(0)))
-
+f = f_h
+a, r = cfc.solveq(K, f, np.array(bdofs.get(0)), np.array(bdofs.get(0))* 0 + T_inf)
 
 cfv.figure(fig_size=(10, 10))
 cfv.draw_nodal_values_shaded(a, coords, edof, title="Temperature",
                             dofs_per_node=1, el_type=2, draw_elements=True)
-
-
-
+cfv.colorbar()
 cfv.showAndWait()
